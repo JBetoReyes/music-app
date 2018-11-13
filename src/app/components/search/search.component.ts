5273635efded536c87d05ae1b2a9385e1096b949
JoseRQ <mac@MacBook-Air-de-MAC.local>
2018-11-12 23:56:44 -0600

    implements the search component

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {SpotifyService} from '../../services/spotify.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styles: []
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent {
 
-  constructor() { }
+  _artists = [];
 
-  ngOnInit() {
+  constructor(private _spotifyService: SpotifyService) { }
+
+  search(searchTerm: string) {
+    console.log(searchTerm);
+    this._spotifyService.getArtists(searchTerm).subscribe((results) => {
+      console.log(results.artists);
+      this._artists = results.artists.items;
+    });
   }
 
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -6,14 +6,24 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 })
 export class SpotifyService {
 
+  TOKEN = 'BQA7NOf0E5cgWa4gDUxFpXwNjvTF4WJqokD9Kb_YGL92U6lj5n8wuZV69XRsCErroyYn0TxlOCGGa4AQflo';
+
   constructor(private _httpClient: HttpClient) { }
 
   getNewReleases() {
 
     const headers = new HttpHeaders({
-      Authorization: 'Bearer BQA7NOf0E5cgWa4gDUxFpXwNjvTF4WJqokD9Kb_YGL92U6lj5n8wuZV69XRsCErroyYn0TxlOCGGa4AQflo',
+      Authorization: `Bearer ${this.TOKEN}`,
     });
 
     return this._httpClient.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers });
+  }
+
+  getArtists(searchTerm) {
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${this.TOKEN}`,
+    });
+
+    return this._httpClient.get(`https://api.spotify.com/v1/search?q=${searchTerm}&type=artist`, { headers });
   }
 }
